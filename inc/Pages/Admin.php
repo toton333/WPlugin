034d1ba9fd64b0bd8ecd55e50738b62d36b67613
toton333 <toton333@yahoo.com>
2018-02-27 23:28:38 +0530

    modular submenu pages

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -9,7 +9,8 @@ class Admin
 {
 
 	public $settings;
-	public $pages;
+	public $pages = array();
+	public $subpages = array();
 
     public function __construct(){
 
@@ -28,10 +29,37 @@ class Admin
 
       ));
 
+      $this->subpages = array(
+			array(
+				'parent_slug' => 'bishan_plugin', 
+				'page_title' => 'Custom Post Types', 
+				'menu_title' => 'CPT', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'bishan_cpt', 
+				'callback' => function() { echo '<h1>CPT Manager</h1>'; }
+			),
+			array(
+				'parent_slug' => 'bishan_plugin', 
+				'page_title' => 'Custom Taxonomies', 
+				'menu_title' => 'Taxonomies', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'bishan_taxonomies', 
+				'callback' => function() { echo '<h1>Taxonomies Manager</h1>'; }
+			),
+			array(
+				'parent_slug' => 'bishan_plugin', 
+				'page_title' => 'Custom Widgets', 
+				'menu_title' => 'Widgets', 
+				'capability' => 'manage_options', 
+				'menu_slug' => 'bishan_widgets', 
+				'callback' => function() { echo '<h1>Widgets Manager</h1>'; }
+			)
+		);
+
     }
 
 	public function register() {
-		$this->settings->addPages($this->pages)->register();
+		$this->settings->addPages( $this->pages )->withSubPage( 'Dashboard' )->addSubPages( $this->subpages )->register();
 	}
 	
 }
