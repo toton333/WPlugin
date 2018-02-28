49a5bda162145d25d2d27906a99e67b058cd678d
toton333 <toton333@yahoo.com>
2018-03-01 00:06:21 +0530

    settings,section,fields

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -4,39 +4,64 @@
  */
 namespace Inc\Pages;
 use \Inc\Api\SettingsApi;
+use \Inc\Api\Callbacks\AdminCallbacks;
 
 class Admin
 {
 
 	public $settings;
+	public $callbacks;
 	public $pages = array();
 	public $subpages = array();
 
     public function __construct(){
 
       $this->settings = new SettingsApi();
-      $this->pages = array(
+      $this->callbacks = new AdminCallbacks();
+     
+
+    }
+
+	public function register() {
+
+		$this->setPages();
+		$this->setSubpages();
+
+		$this->setSettings();
+		$this->setSections();
+		$this->setFields();
+
+		$this->settings->addPages( $this->pages )->withSubPage( 'Dashboard' )->addSubPages( $this->subpages )->register();
+	}
+
+	public function setPages(){
+
+     $this->pages = array(
       	array(
 
       		'page_title' => 'Bishan Plugin', 
 			'menu_title' => 'Bishan', 
 			'capability' => 'manage_options', 
 			'menu_slug' => 'bishan_plugin', 
-			'callback' => function() { echo '<h1>Bishan Settings Page</h1>'; }, 
+			'callback' => array( $this->callbacks, 'adminDashboard' ), 
 			'icon_url' => 'dashicons-store', 
 			'position' => 110
 
 
       ));
 
-      $this->subpages = array(
+	}
+
+	public function setSubpages(){
+
+     $this->subpages = array(
 			array(
 				'parent_slug' => 'bishan_plugin', 
 				'page_title' => 'Custom Post Types', 
 				'menu_title' => 'CPT', 
 				'capability' => 'manage_options', 
 				'menu_slug' => 'bishan_cpt', 
-				'callback' => function() { echo '<h1>CPT Manager</h1>'; }
+				'callback' => array( $this->callbacks, 'adminCpt' )
 			),
 			array(
 				'parent_slug' => 'bishan_plugin', 
@@ -44,7 +69,7 @@ class Admin
 				'menu_title' => 'Taxonomies', 
 				'capability' => 'manage_options', 
 				'menu_slug' => 'bishan_taxonomies', 
-				'callback' => function() { echo '<h1>Taxonomies Manager</h1>'; }
+				'callback' =>  array( $this->callbacks, 'adminTaxonomy' )
 			),
 			array(
 				'parent_slug' => 'bishan_plugin', 
@@ -52,14 +77,72 @@ class Admin
 				'menu_title' => 'Widgets', 
 				'capability' => 'manage_options', 
 				'menu_slug' => 'bishan_widgets', 
-				'callback' => function() { echo '<h1>Widgets Manager</h1>'; }
+				'callback' => array( $this->callbacks, 'adminWidget' )
 			)
 		);
-
-    }
-
-	public function register() {
-		$this->settings->addPages( $this->pages )->withSubPage( 'Dashboard' )->addSubPages( $this->subpages )->register();
 	}
+
+
+
+
+	public function setSettings()
+	{
+		$args = array(
+			array(
+				'option_group' => 'bishan_options_group',
+				'option_name' => 'text_example',
+				'callback' => array( $this->callbacks, 'bishanOptionsGroup' )
+			),
+			array(
+				'option_group' => 'bishan_options_group',
+				'option_name' => 'first_name'
+			)
+		);
+		$this->settings->setSettings( $args );
+	}
+
+	public function setSections()
+	{
+		$args = array(
+			array(
+				'id' => 'bishan_admin_index',
+				'title' => 'Settings',
+				'callback' => array( $this->callbacks, 'bishanAdminSection' ),
+				'page' => 'bishan_plugin'
+			)
+		);
+		$this->settings->setSections( $args );
+	}
+
+	public function setFields()
+	{
+		$args = array(
+			array(
+				'id' => 'text_example',
+				'title' => 'Text Example',
+				'callback' => array( $this->callbacks, 'bishanTextExample' ),
+				'page' => 'bishan_plugin',
+				'section' => 'bishan_admin_index',
+				'args' => array(
+					'label_for' => 'text_example',
+					'class' => 'example-class'
+				)
+			),
+			array(
+				'id' => 'first_name',
+				'title' => 'First Name',
+				'callback' => array( $this->callbacks, 'bishanFirstName' ),
+				'page' => 'bishan_plugin',
+				'section' => 'bishan_admin_index',
+				'args' => array(
+					'label_for' => 'first_name',
+					'class' => 'example-class'
+				)
+			)
+		);
+		$this->settings->setFields( $args );
+	}
+
+
 	
 }
--- templates/admin.php
+++ templates/admin.php
@@ -1,1 +1,12 @@
-<h1>Bishan Plugin</h1>
+<div class="wrap">
+	<h1>Bishan Plugin</h1>
+	<?php settings_errors(); ?>
+
+	<form method="post" action="options.php">
+		<?php 
+			settings_fields( 'bishan_options_group' );
+			do_settings_sections( 'bishan_plugin' );
+			submit_button();
+		?>
+	</form>
+</div>
