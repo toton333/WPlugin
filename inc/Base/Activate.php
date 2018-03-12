9125690e4eaa6cca3d539c9ebc112df8b9435259
toton333 <toton333@yahoo.com>
2018-03-13 00:14:37 +0530

    save default options on activate

--- inc/Base/Activate.php
+++ inc/Base/Activate.php
@@ -9,5 +9,12 @@ class Activate
 {
 	public static function activate() {
 		flush_rewrite_rules();
+
+		if ( get_option( 'bishan_plugin' ) ) {
+			return;
+		}
+
+		$default = array();
+		update_option( 'bishan_plugin', $default );
 	}
 }
