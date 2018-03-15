9ce6afc8bbec2b6d2463097273b731656513be83
toton333 <toton333@yahoo.com>
2018-03-16 00:34:11 +0530

    initial CPT manager

--- inc/Base/CustomPostTypeController.php
+++ inc/Base/CustomPostTypeController.php
@@ -5,22 +5,35 @@
 namespace Inc\Base;
 use Inc\Api\SettingsApi;
 use Inc\Base\BaseController;
+use Inc\Api\Callbacks\CptCallbacks;
 use Inc\Api\Callbacks\AdminCallbacks;
 /**
 * 
 */
 class CustomPostTypeController extends BaseController
 {
+	
+	public $settings;
 	public $callbacks;
+	public $cpt_callbacks;
 	public $subpages = array();
 	public $custom_post_types = array();
+
 	public function register()
 	{
 		if ( ! $this->activated( 'cpt_manager' ) ) return;
+
+		//populating CPT subpage
 		$this->settings = new SettingsApi();
 		$this->callbacks = new AdminCallbacks();
+		$this->cpt_callbacks = new CptCallbacks();
 		$this->setSubpages();
+		$this->setSettings();
+		$this->setSections();
+		$this->setFields();
 		$this->settings->addSubPages( $this->subpages )->register();
+
+		//creating custom posts based on CPT subpage
 		$this->storeCustomPostTypes();
 		if ( ! empty( $this->custom_post_types ) ) {
 			add_action( 'init', array( $this, 'registerCustomPostTypes' ) );
@@ -39,54 +52,153 @@ class CustomPostTypeController extends BaseController
 			)
 		);
 	}
+
+	public function setSettings()
+	{
+		$args = array(
+			array(
+				'option_group' => 'bishan_plugin_cpt_settings',
+				'option_name' => 'bishan_plugin_cpt',
+				'callback' => array( $this->cpt_callbacks, 'cptSanitize' )
+			)
+		);
+		$this->settings->setSettings( $args );
+	}
+
+	public function setSections()
+	{
+		$args = array(
+			array(
+				'id' => 'bishan_cpt_index',
+				'title' => 'Custom Post Type Manager',
+				'callback' => array( $this->cpt_callbacks, 'cptSectionManager' ),
+				'page' => 'bishan_cpt'
+			)
+		);
+		$this->settings->setSections( $args );
+	}
+
+	public function setFields()
+	{
+		$args = array(
+			array(
+				'id' => 'post_type',
+				'title' => 'Custom Post Type ID',
+				'callback' => array( $this->cpt_callbacks, 'textField' ),
+				'page' => 'bishan_cpt',
+				'section' => 'bishan_cpt_index',
+				'args' => array(
+					'option_name' => 'bishan_plugin_cpt',
+					'label_for' => 'post_type',
+					'placeholder' => 'eg. product'
+				)
+			),
+			array(
+				'id' => 'singular_name',
+				'title' => 'Singular Name',
+				'callback' => array( $this->cpt_callbacks, 'textField' ),
+				'page' => 'bishan_cpt',
+				'section' => 'bishan_cpt_index',
+				'args' => array(
+					'option_name' => 'bishan_plugin_cpt',
+					'label_for' => 'singular_name',
+					'placeholder' => 'eg. Product'
+				)
+			),
+			array(
+				'id' => 'plural_name',
+				'title' => 'Plural Name',
+				'callback' => array( $this->cpt_callbacks, 'textField' ),
+				'page' => 'bishan_cpt',
+				'section' => 'bishan_cpt_index',
+				'args' => array(
+					'option_name' => 'bishan_plugin_cpt',
+					'label_for' => 'plural_name',
+					'placeholder' => 'eg. Products'
+				)
+			),
+			array(
+				'id' => 'public',
+				'title' => 'Public',
+				'callback' => array( $this->cpt_callbacks, 'checkboxField' ),
+				'page' => 'bishan_cpt',
+				'section' => 'bishan_cpt_index',
+				'args' => array(
+					'option_name' => 'bishan_plugin_cpt',
+					'label_for' => 'public',
+					'class' => 'ui-toggle'
+				)
+			),
+			array(
+				'id' => 'has_archive',
+				'title' => 'Archive',
+				'callback' => array( $this->cpt_callbacks, 'checkboxField' ),
+				'page' => 'bishan_cpt',
+				'section' => 'bishan_cpt_index',
+				'args' => array(
+					'option_name' => 'bishan_plugin_cpt',
+					'label_for' => 'has_archive',
+					'class' => 'ui-toggle'
+				)
+			)
+		);
+		$this->settings->setFields( $args );
+	}
+
+
+
+
 	public function storeCustomPostTypes()
 	{
+
+		$options = get_option('bishan_plugin_cpt');
+
 		$this->custom_post_types[] = array(
-			'post_type'             => 'test',
-			'name'                  => '',
-			'singular_name'         => '',
-			'menu_name'             => '',
-			'name_admin_bar'        => '',
-			'archives'              => '',
-			'attributes'            => '',
-			'parent_item_colon'     => '',
-			'all_items'             => '',
-			'add_new_item'          => '',
-			'add_new'               => '',
-			'new_item'              => '',
-			'edit_item'             => '',
-			'update_item'           => '',
-			'view_item'             => '',
-			'view_items'            => '',
-			'search_items'          => '',
-			'not_found'             => '',
-			'not_found_in_trash'    => '',
-			'featured_image'        => '',
-			'set_featured_image'    => '',
-			'remove_featured_image' => '',
-			'use_featured_image'    => '',
-			'insert_into_item'      => '',
-			'uploaded_to_this_item' => '',
-			'items_list'            => '',
-			'items_list_navigation' => '',
-			'filter_items_list'     => '',
-			'label'                 => '',
-			'description'           => '',
-			'supports'              => false,
-			'taxonomies'            => array(),
-			'hierarchical'          => false,
-			'public'                => true,
-			'show_ui'               => true,
-			'show_in_menu'          => true,
-			'menu_position'         => 5,
-			'show_in_admin_bar'     => true,
-			'show_in_nav_menus'     => true,
-			'can_export'            => true,
-			'has_archive'           => true,
-			'exclude_from_search'   => false,
-			'publicly_queryable'    => true,
-			'capability_type'       => 'page'
-		);
+				'post_type'             => $options['post_type'],
+				'name'                  => $options['plural_name'],
+				'singular_name'         => $options['singular_name'],
+				'menu_name'             => $options['plural_name'],
+				'name_admin_bar'        => $options['singular_name'],
+				'archives'              => $options['singular_name'] . ' Archives',
+				'attributes'            => $options['singular_name'] . ' Attributes',
+				'parent_item_colon'     => 'Parent ' . $options['singular_name'],
+				'all_items'             => 'All ' . $options['plural_name'],
+				'add_new_item'          => 'Add New ' . $options['singular_name'],
+				'add_new'               => 'Add New',
+				'new_item'              => 'New ' . $options['singular_name'],
+				'edit_item'             => 'Edit ' . $options['singular_name'],
+				'update_item'           => 'Update ' . $options['singular_name'],
+				'view_item'             => 'View ' . $options['singular_name'],
+				'view_items'            => 'View ' . $options['plural_name'],
+				'search_items'          => 'Search ' . $options['plural_name'],
+				'not_found'             => 'No ' . $options['singular_name'] . ' Found',
+				'not_found_in_trash'    => 'No ' . $options['singular_name'] . ' Found in Trash',
+				'featured_image'        => 'Featured Image',
+				'set_featured_image'    => 'Set Featured Image',
+				'remove_featured_image' => 'Remove Featured Image',
+				'use_featured_image'    => 'Use Featured Image',
+				'insert_into_item'      => 'Insert into ' . $options['singular_name'],
+				'uploaded_to_this_item' => 'Upload to this ' . $options['singular_name'],
+				'items_list'            => $options['plural_name'] . ' List',
+				'items_list_navigation' => $options['plural_name'] . ' List Navigation',
+				'filter_items_list'     => 'Filter' . $options['plural_name'] . ' List',
+				'label'                 => $options['singular_name'],
+				'description'           => $options['plural_name'] . 'Custom Post Type',
+				'supports'              => array( 'title', 'editor', 'thumbnail' ),
+				'taxonomies'            => array( 'category', 'post_tag' ),
+				'hierarchical'          => false,
+				'public'                => $options['public'],
+				'show_ui'               => true,
+				'show_in_menu'          => true,
+				'menu_position'         => 5,
+				'show_in_admin_bar'     => true,
+				'show_in_nav_menus'     => true,
+				'can_export'            => true,
+				'has_archive'           => $options['has_archive'],
+				'exclude_from_search'   => false,
+				'publicly_queryable'    => true,
+				'capability_type'       => 'post'
+			);
 	}
 	public function registerCustomPostTypes()
 	{
--- templates/cpt.php
+++ templates/cpt.php
@@ -1,3 +1,14 @@
 
+	<div class="wrap">
 	<h1>CPT Manager</h1>
+	<?php settings_errors(); ?>
+
+	<form method="post" action="options.php">
+		<?php 
+			settings_fields( 'bishan_plugin_cpt_settings' );
+			do_settings_sections( 'bishan_cpt' );
+			submit_button();
+		?>
+	</form>
+</div>
 	
