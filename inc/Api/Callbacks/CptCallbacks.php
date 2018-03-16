57dca46ca06879a12663e76c2d5787aa51e692f2
toton333 <toton333@yahoo.com>
2018-03-16 13:24:54 +0530

    creating multiple custom post type

--- inc/Api/Callbacks/CptCallbacks.php
+++ inc/Api/Callbacks/CptCallbacks.php
@@ -11,15 +11,31 @@ class CptCallbacks
 	}
 	public function cptSanitize( $input )
 	{
-		return $input;
+		$output = get_option( 'bishan_plugin_cpt');
+        foreach ($output as $key => $value) {
+        	if($input['post_type'] === $key){
+
+        		$output[$key] = $input;
+
+        	}else{
+
+        		$output[$input['post_type']] = $input;
+
+        	}
+        }
+
+
+       return $output;
 	}
+
+
 	public function textField( $args )
 	{
 		$name = $args['label_for'];
 		$option_name = $args['option_name'];
 		$input = get_option( $option_name );
-		$value = $input[$name];
-		echo '<input type="text" class="regular-text" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="' . $value . '" placeholder="' . $args['placeholder'] . '">';
+		
+				echo '<input type="text" class="regular-text" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="" placeholder="' . $args['placeholder'] . '">';
 	}
 	public function checkboxField( $args )
 	{
@@ -27,7 +43,7 @@ class CptCallbacks
 		$classes = $args['class'];
 		$option_name = $args['option_name'];
 		$checkbox = get_option( $option_name );
-		$checked = isset($checkbox[$name]) ? ($checkbox[$name] ? true : false) : false;
-		echo '<div class="' . $classes . '"><input type="checkbox" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="1" class="" ' . ( $checked ? 'checked' : '') . '><label for="' . $name . '"><div></div></label></div>';
+		echo '<div class="' . $classes . '"><input type="checkbox" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="1" class=""><label for="' . $name . '"><div></div></label></div>';
+	
 	}
 }
